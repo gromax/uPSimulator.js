6a849094ed0e19a5f52e0d4d03a2f6be860735bc
Maxence Klein <maxence.klein@goupill.fr>
2024-02-08 19:31:03 +0100

    ajout commentaires et simplification lien vers simu

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import { AsmLines } from './compile/parseasm';
 
 
 function submitPython(){
+    /* prend le contenu de la boîte Python,
+       compile et produit l'asm dans la boîte Asm
+       renvoie un message journal
+    */
     let source = pythonSource.value;
     addMessage("Compilation &rarr; ASM...", false, 'Python');
     try {
@@ -21,35 +25,50 @@ function submitPython(){
 }
 
 function runPython(){
+    /* prend le contenu de la boîte python
+       tente la compilation asm,
+       puis la traduction en binaire
+       et envoie l'exécution
+       les logs sont envoyés dans la même boîte
+       en cas de succès, envoie la fenêtre de simulation avec la clé contenant le source
+    */
     let p = submitPython();
     if (p==null) {
         return null;
     }
-    let a = submitAsm();
+    let a = submitAsm('Python');
     if (a==null) {
         return null;
     }
+    // succès, le python peut être envoyé
     let source = encodeURIComponent(pythonSource.value);
-    let asmCode = encodeURIComponent(asm.value);
-    let hex = binary.hex;
-    let pNumbers = encodeURIComponent(p.linesNumbers.join('_'));
-    let aNumbers = encodeURIComponent(a.linesNumbers.join('_'));
-    window.open(`./up.html?hex=${hex}&source=${source}&asm=${asmCode}&p=${pNumbers}&a=${aNumbers}`, '_blank').focus();
+    //let asmCode = encodeURIComponent(asm.value);
+    //let hex = binary.hex;
+    //let pNumbers = encodeURIComponent(p.linesNumbers.join('_'));
+    //let aNumbers = encodeURIComponent(a.linesNumbers.join('_'));
+    window.open(`./up.html?python=${source}`, '_blank').focus();
 }
 
-function submitAsm(){
+function submitAsm(cibleLog = 'Asm'){
+    /* cibleLog: boîte d'erreur dans laquelle on écrit 'Asm' ou 'Python'
+       prend le contenu de la boite asm, traduit en binaire
+       change le contenu de la boîte binaire
+       ajoute un message journal dans la cible
+    */
+    if (cibleLog != 'Python') {
+        cibleLog = 'Asm';
+    }
     let source = asm.value;
-    addMessage("Codage ASM &rarr; Binaire...", false, 'Asm');
+    addMessage("Codage ASM &rarr; Binaire...", false, cibleLog);
     try {
         let asm = new AsmLines(source);
         let b = asm.binary;
         binary.value = b.join('\n');
         binary.hex = asm.hex.join('');
-        addMessage("Succès !", 'validline', 'Asm');
-        //engine.load(asm.program);
+        addMessage("Succès !", 'validline', cibleLog);
         return asm;
     } catch({type, message}) {
-        addMessage(message, 'errorline', 'Asm');
+        addMessage(message, 'errorline', cibleLog);
         return null;
     }
 }
