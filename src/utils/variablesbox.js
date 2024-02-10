df78c9485af7ad7da814359e0297c644dc7e8dc8
Maxence Klein <maxence.klein@goupill.fr>
2024-02-10 23:16:26 +0100

    hérite de box. Si vide, est zombie

--- src/utils/variablesbox.js
+++ src/utils/variablesbox.js
@@ -1,36 +1,24 @@
 import { decToRadix } from './misc';
+import { Box } from './box';
 
-class VBox {
+class VBox extends Box {
     #valuesNodes;
-    #container;
-    #titlebar;
-    #x;
-    #y;
-    #mouseUpHandler;
-    #mouseMoveHandler;
-    #button;
-    #content;
 
-    constructor(variables, container) {
+    constructor(variables) {
         /* variables: dictionnaire de forme {nom:{line, value}}
            container: div contenant la boîte
         */
-        this.#container = container;
-        this.#container.classList.add('vboxcontainer');
-        this.#titlebar = document.createElement("div");
-        this.#button = document.createElement("button");
-        this.#button.classList.add('boxbutton');
-        this.#button.innerText = "□";
-        this.#titlebar.appendChild(this.#button);
-        this.#titlebar.appendChild(document.createTextNode('  Variables'));
-        this.#container.appendChild(this.#titlebar);
-        this.#titlebar.classList.add('vboxtitle');
-        this.#content = document.createElement("table");
-        //this.#content.classList.add('boxcontent');
-        this.#content.innerHTML = "<tr><th>nom</th><th>@</th><th>Valeur</th></tr>";
-        this.#content.classList.add('vbox');
-        this.#container.appendChild(this.#content);
+        let length = Object.keys(variables).length;
+        super('Variables', "table", length == 0);
         this.#valuesNodes = {};
+        if (length == 0) {
+            return;
+        }
+        {
+            let row = document.createElement('tr');
+            row.innerHTML = "<th>nom</th><th>@</th><th>Valeur</th>";
+            this.append(row);
+        }
         for (let name in variables){
             let adresse = variables[name].line;
             let value = variables[name].value;
@@ -45,48 +33,8 @@ class VBox {
             this.#valuesNodes[adresse] = cell;
             cell.appendChild(document.createTextNode(decToRadix(value,16,4)));
             row.appendChild(cell);
-            this.#content.appendChild(row);
+            this.append(row);
         }
-        let that = this;
-        this.#titlebar.addEventListener("mousedown", function(e) { that.mouseDown(e); });
-        this.#button.addEventListener("click", function(e) { that.reduce(e); });
-    }
-
-    mouseDown(e){
-        this.#x = e.clientX;
-        this.#y = e.clientY;
-        let that = this;
-        this.#mouseMoveHandler = function(e) {that.mouseMove(e);};
-        this.#mouseUpHandler = function(e) {that.mouseUp(e);};
-        document.addEventListener("mousemove", this.#mouseMoveHandler);
-        document.addEventListener("mouseup", this.#mouseUpHandler);
-    }
-
-    mouseMove(e){
-        let dx = e.clientX - this.#x;
-        let dy = e.clientY - this.#y;
-        this.move(dx, dy);
-        this.#x = e.clientX;
-        this.#y = e.clientY;
-    }
-
-    mouseUp(e){
-        document.removeEventListener("mouseup", this.#mouseUpHandler);
-        document.removeEventListener("mousemove", this.#mouseMoveHandler);
-    }
-
-    reduce(e) {
-        this.#content.classList.toggle('invisible');
-    }
-
-    move(dx, dy) {
-        this.#container.style.left = (this.#container.offsetLeft  + dx) + 'px';
-        this.#container.style.top = (this.#container.offsetTop + dy) + 'px';
-    }
-
-    setXY(x,y) {
-        this.#container.style.left = x + 'px';
-        this.#container.style.top = y + 'px';
     }
 
     get variablesNodes(){
